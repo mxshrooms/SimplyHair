dce2a4d5bff6c35ffcdf8c73462a2b15f4b5c53d
mxshrooms <104537309+mxshrooms@users.noreply.github.com>
2023-09-15 19:39:48 +1200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+function home() {
+    window.location = "index.html";
+}
+function shop() {
+    window.location = "shop.html";
+}
+function contact() {
+    window.location = "contact.html";
+}
+function about() {
+    window.location = "about.html";
+}
+function shop2() {
+    window.location = "shop.html";
+}
+function cart() {
+    window.location = "cart.html";
+}
+function checkoutfunc() {
+    window.location = "checkout.html";
+}
+function confirmorder() {
+    window.location = "thankyou.html";
+}
